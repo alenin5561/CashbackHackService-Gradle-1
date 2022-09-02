c77f2cb1726c2a81c55347d0dfdb3fea29a7d6c8
Аленин Андрей <aleninmailbox@gmail.com>
2022-09-02 12:00:07 +0300

    Service bug

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -27,14 +27,4 @@ public class CashbackHackServiceTest {
         assertEquals(actual,expected);
 
     }
-    @Test
-    public void testRemain3() {
-        CashbackHackService service = new CashbackHackService();
-
-        int expected = 0;
-        int actual = service.remain(1000);
-
-        assertEquals(actual,expected);
-
-    }
 }
